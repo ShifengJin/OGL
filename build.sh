ccb7ecfe881962e441ae053e3f5925ccd1f878b7
ShifengJin <921942722@qq.com>
2023-07-25 18:54:21 -0700

    add build.sh

---
+++ build.sh
@@ -0,0 +1,16 @@
+cd ThirdLibrary/assimp
+
+mkdir build
+cd build
+sudo rm * -rf
+cmake .. -DCMAKE_BUILD_TYPE=Release -DASSIMP_INSTALL=OFF -DASSIMP_BUILD_TESTS=OFF -DASSIMP_INJECT_DEBUG_POSTFIX=OFF -DASSIMP_BUILD_ZLIB=OFF
+make -j16
+cp include/assimp ../../../Assimp -rf
+cp ../include/assimp/ ../../../Assimp -rf
+cp ../contrib ../../../Assimp -rf
+cp bin/* ../../../Assimp/lib
+cd ../../../
+mkdir build
+cd build
+cmake ..
+make -j16
